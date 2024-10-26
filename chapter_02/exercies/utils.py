d40bc8983833473b4c8e1fc4f1bc244ea49a814a
Tevin.Achong <tevin.achong@shell.com>
2024-10-26 17:10:30 -0400

    cleaned chapter 2 notebook

--- chapter_02/exercies/utils.py
+++ chapter_02/exercies/utils.py
@@ -3,5 +3,15 @@ import pandas as pd
 import tarfile
 import urllib.request
 
-def printHey():
-    print("Hey")
+def load_housing_data():
+    '''
+    Returns: a pandas dataframe containing the housing data
+    '''
+    tarball_path = Path("datasets/housing.tgz")
+    if not tarball_path.is_file():
+        Path("datasets").mkdir(parents=True, exist_ok=True)
+        url = "https://github.com/ageron/data/raw/main/housing.tgz"
+        urllib.request.urlretrieve(url, tarball_path)
+        with tarfile.open(tarball_path) as housing_tarball:
+            housing_tarball.extractall(path="datasets")
+    return pd.read_csv(Path("datasets/housing/housing.csv"))
